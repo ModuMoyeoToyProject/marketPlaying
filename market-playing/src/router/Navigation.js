7c1bd2d2ad7147d21824d844ef230e94d81c9274
리코더 <oooley200@gmail.com>
2022-10-07 17:54:57 +0900

    pageList 생성

--- market-playing/src/router/Navigation.js
+++ market-playing/src/router/Navigation.js
@@ -2,20 +2,24 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "../pages/Home";
 import Shop from "../pages/Shop";
-import Chart from "../pages/Chart";
-import UserInfoTest from "../pages/UserInfoTest";   
+import Chart from "../pages/Chart";  
 import Street from "../pages/Street";
+import GameMain from "../pages/GameMain";
+import PageList from "../pages/PageList";   
+import UserInfoTest from "../pages/UserInfoTest";  
 import "../styles/App.scss";
 
 function Navigation() {
   return (
     <Router className="main">
       <Routes>
-        <Route exact path="/" element={<Home />} />
-        <Route path="/userInfoTest" element={<UserInfoTest />} />
+        <Route exact path="/" element={<PageList />} /> 
+        <Route path="/lake" element={<Home />} />
         <Route path="/shop" element={<Shop />} />
         <Route path="/chart" element={<Chart />} />
         <Route path="/street" element={<Street />} />
+        <Route path="/main" element={<GameMain />} />
+        <Route path="/userInfoTest" element={<UserInfoTest />} />
       </Routes>
     </Router>
   );
