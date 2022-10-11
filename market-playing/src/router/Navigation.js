d351a877b01ed9be6fd96c0364b87b41bcacd235
하돌 <102174146+jinyoung4478@users.noreply.github.com>
2022-10-12 01:14:54 +0900

    feat: 맵 이동하기 기능 구현

--- market-playing/src/router/Navigation.js
+++ market-playing/src/router/Navigation.js
@@ -2,18 +2,18 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "../pages/Home";
 import Shop from "../pages/Shop";
-import Chart from "../pages/Chart";  
+import Chart from "../pages/Chart";
 import Street from "../pages/Street";
 import GameMain from "../pages/GameMain";
-import PageList from "../pages/PageList";   
-import UserInfoTest from "../pages/UserInfoTest";  
+import PageList from "../pages/PageList";
+import UserInfoTest from "../pages/UserInfoTest";
 import "../styles/App.scss";
 
 function Navigation() {
   return (
     <Router className="main">
       <Routes>
-        <Route exact path="/" element={<PageList />} /> 
+        <Route exact path="/" element={<PageList />} />
         <Route path="/lake" element={<Home />} />
         <Route path="/shop" element={<Shop />} />
         <Route path="/chart" element={<Chart />} />
