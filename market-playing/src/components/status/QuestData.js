fd8457376da9906e3fe5e9eb3453092c1196cbf6
리코더 <oooley200@gmail.com>
2023-01-05 01:01:12 +0900

    퀘스트 추가수정

--- market-playing/src/components/status/QuestData.js
+++ market-playing/src/components/status/QuestData.js
@@ -1,7 +1,16 @@
 import React, { useState, useRef, useEffect } from "react";
 
+// import celery from '../../img/neighbor/celery.jpeg';
+// import egg from '../../img/neighbor/egg.jpeg';
+// import onion from '../../img/neighbor/onion.jpeg';
+// import riceball from '../../img/neighbor/riceball.jpeg';
+// import pumpkin from '../../img/neighbor/pumpkin.jpeg';
+// import mushroom from '../../img/neighbor/mushroom.jpeg';
+
 
 const QuestData = (props) => { 
+
+    const [selectTab, setSelectTab] = useState("A");
 
     const questData = [ 
       // id 
@@ -9,7 +18,9 @@ const QuestData = (props) => {
       // , status // 퀘스트 완료 여부 - 진행전 / 완료 S/ 포기 F
       // , name // 퀘스트 명 
       // , contents // 퀘스트 내용
-      // , from // 요청 주민
+      // , fromId // 요청 주민 id
+      // , fromName // 요청 주민 name
+      // , fromImg // 요청 주민 Img
       // , missions // 퀘스트 요구사항
       // , reword // 퀘스트 보상  
     {
@@ -18,10 +29,13 @@ const QuestData = (props) => {
       , status: ''  
       , name: '마을을 어지럽히는 늑대 사냥 '  
       , contents: '요즘 늑대가 우리 집 양들을 자꾸 잡아먹어서 난리야. 어제도 세 마리나 손해 봤다니까! 어떻게 해결할 방법 없을까?' 
-      , from: '주민1'  
+      , fromId: 'onion'  
+      , fromName: '양파 쿵야'
+      , fromImg: require("../../img/neighbor/onion.png")
       , missions: [{item:'늑대', amount:50, count:4}]  
-      , reword: [{name:'초급 체력 물약 x 10', item:'초급 체력 물약', amount:10}
-                  ,{name:'5300M', item:'M', amount:5300}]  
+      , reword: [{name:'초급 체력 물약 x 10', item:'초급 체력 물약', amount:10, img:require('../../img/liquid.jpeg')}
+                  ,{name:'기본 짚신 ', item:'짚신', amount:1, img:''} 
+                  ,{name:'5300M', item:'M', amount:5300, img:require('../../img/coin.png')}]  
     }
     ,{
       id: 'q2'
@@ -29,13 +43,14 @@ const QuestData = (props) => {
       , status: 'S'  
       , name: '모그호크 고기는 맛있어'  
       , contents: '모그호크 고기가 별미라더라. 어디가서 찾아야지? ' 
-      , from: '주민2'  
+      , fromId: 'riceball'  
+      , fromName: '주먹밥 쿵야'
+      , fromImg: require("../../img/neighbor/riceBall.jpeg")
       , missions: [{item:'모그호크 고기', amount:10, count:10}]  
-      , reword: [{name:'초급 체력 물약 x 10', item:'초급 체력 물약', amount:10}
-                  ,{name:'530M', item:'M', amount:530}
-                  ,{name:'기본 짚신 ', item:'짚신', amount:1}
-                  ,{name:'경험치', item:'경험치', amount:13000}
-                  ,{name:'아이템1 x 3', item:'아이템1', amount:3}]  
+      , reword: [{name:'초급 체력 물약 x 10', item:'초급 체력 물약', amount:10, img:require('../../img/liquid.jpeg')}
+                  ,{name:'530M', item:'M', amount:530, img:require('../../img/coin.png')}
+                  ,{name:'황금가위', item:'황금가위', amount:1, img:require('../../img/gold_scissor.jpeg')} 
+                  ,{name:'아이템1 x 3', item:'아이템1', amount:3, img:''}] 
     }
     ,{
       id: 'q3'
@@ -43,10 +58,13 @@ const QuestData = (props) => {
       , status: ''  
       , name: '그리무츠는 사실 착한 몬스터? '  
       , contents: '착한 몬스터가 맞는지 확인 부탁해' 
-      , from: '주민3'  
+      , fromId: 'celery'  
+      , fromName: '셀러리 쿵야'
+      , fromImg: require("../../img/neighbor/celery.jpeg")
       , missions: [{item:'그리무츠와 우정반지', amount:1, count:0}]  
-      , reword: [{name:'초급 체력 물약', item:'초급 체력 물약', amount:10}
-                  ,{name:'5300M', item:'M', amount:5300}]  
+      , reword: [{name:'초급 체력 물약', item:'초급 체력 물약', amount:10, img:require('../../img/liquid.jpeg')}
+                  ,{name:'경험치', item:'경험치', amount:13000, img:''}
+                  ,{name:'5300M', item:'M', amount:5300, img:require('../../img/coin.png')}]  
     }
     ];
 
@@ -54,6 +72,7 @@ const QuestData = (props) => {
     const [detailData, setdetailData] = useState(false); 
 
     const listFilter = (keyword) => {
+      setSelectTab(keyword);
       let list = [];
       if( keyword && keyword == 'A' ){
         list = questData; 
@@ -83,27 +102,27 @@ const QuestData = (props) => {
   <div className="quest">
     <div className="quest_list">
       <div className="tablist"> 
-        <div className="quest_tab on"
+        <div className={"quest_tab"+(selectTab=="A"?" on":"")}
             onClick={() => listFilter("A")}
         > 
-        {"전체"}
+        <span>{"전체"}</span>
         </div>
-        <div className="quest_tab"
+        <div className={"quest_tab"+(selectTab=="N"?" on":"")}
             onClick={() => listFilter("N")}
         > 
-        {"일반"}
+        <span>{"일반"}</span>
         </div>
-        <div className="quest_tab"
+        <div className={"quest_tab"+(selectTab=="I"?" on":"")}
             onClick={() => listFilter("I")}
         > 
-        {"인던"}
+        <span>{"인던"}</span>
         </div>
       </div>
       <div className="list">
         <table className="list_table">
           <tbody>
             <colgroup>
-              <col style={{width: '50px'}}/>
+              <col style={{width: '20px'}}/>
               <col />
             </colgroup>
             {questList.map((el, ind) => ( 
@@ -112,11 +131,11 @@ const QuestData = (props) => {
               key={"tr_"+el.id}
               onClick={() => questDetail(el.id)}
               >
-                <td>
-                  {el.status}
+                <td align="center">
+                {el.status=="S"?"완료":""}
                 </td>
                 <td>
-                  {el.name}
+                <span>{el.name}</span> 
                 </td>
               </tr>
               </>
@@ -125,13 +144,25 @@ const QuestData = (props) => {
         </table>
       </div> 
     </div>
-    <div className="quest_detail">
-      <div className="quest_contents">
-        {detailData?.name}
-        {"\n\n"}  
-        {detailData?.contents}  
-        {"\n\n"}  
+    <div className="quest_detail"> 
+      <div className="quest_contents">  
+        <div className="quest_header">
+          <div className="quest_from">
+            <img className="neighbor" src={detailData?.fromImg} />
+          </div>
+          <div className="quest_title">
+          {detailData?.name}
+          </div>
+        </div>
+        <div className="quest_detail">
+          {detailData?.contents}  
+        </div> 
+        <div className="quest_mission">
         <table>
+            <colgroup>
+              <col />
+              <col style={{width: '100px'}}/>
+            </colgroup>
         {detailData?.missions?.map((el, ind) => ( 
           <tr>
             <td>{el.item}</td>
@@ -139,19 +170,37 @@ const QuestData = (props) => {
           </tr>
         ))}
         </table> 
+        </div> 
       </div>
-      <div className="quest_reward">
-        <div className="reword">
-        {detailData?.from}  
-        {"\n\n"}  
-        <table>
-        {detailData?.reword?.map((el, ind) => ( 
-          <tr>
-            <td>{el.name}</td> 
-          </tr>
-        ))}
-        </table> 
+      <div className="quest_reward"> 
+        <div className="reword_title"> 
+        <span>{"퀘스트 보상"}</span>
         </div>
+        <div className="reword_list"> 
+          {/* {detailData?.reword?.map((el, ind) => (  
+            <div className="reword_item">
+              <div className="reword_img">
+              <img src={el.img} />
+              </div>
+              <div className="reword_detail">
+                <span>{el.name}</span> 
+              </div>
+
+            </div>
+          ))}  */}
+          <table>
+            {detailData?.reword?.map((el, ind) => (  
+              <tr className="reword_item">
+                <td className="reword_img">
+                <img src={el.img} />
+                </td>
+                <td className="reword_detail">
+                  <span>{el.name}</span> 
+                </td>
+              </tr>
+            ))} 
+          </table>
+        </div>  
       </div>
     </div>
   </div>
