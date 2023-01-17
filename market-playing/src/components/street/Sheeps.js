c20413f8c673344903ebf394c0bdd6e1ce3c5cfe
jiae <yja2397@gmail.com>
2023-01-17 23:07:27 +0900

    workplace

--- market-playing/src/components/street/Sheeps.js
+++ market-playing/src/components/street/Sheeps.js
@@ -2,31 +2,24 @@
 // import styled from 'styled-components';
 // import kirby from '../../img/sheep_1';
 
-import Sheep from './Sheep';
- 
+import Sheep from "./Sheep";
 
-const Sheeps = props => { 
-   return (
-      <>
+const Sheeps = (props) => {
+  return (
+    <>
       <div className={"sheepArea"}>
-         <div className={"sheep1"}>
-            <Sheep
-               sheepNm="sheep1"
-            ></Sheep>
-         </div> 
-         <div className={"sheep2"}>
-            <Sheep
-               sheepNm="sheep2"
-            ></Sheep>
-         </div> 
-         <div className={"sheep3"}>
-            <Sheep
-               sheepNm="sheep3"
-            ></Sheep>
-         </div> 
-      </div> 
-      </>
-   );
+        <div className={"sheep1"}>
+          <Sheep sheepNm="sheep1"></Sheep>
+        </div>
+        <div className={"sheep2"}>
+          <Sheep sheepNm="sheep2"></Sheep>
+        </div>
+        <div className={"sheep3"}>
+          <Sheep sheepNm="sheep3"></Sheep>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Sheeps;
